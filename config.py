eedc7c69c9a1d9249d09a1623fcf436e874fbc8d
ManantsoaHerivola <raharimanantsoaherivola@gmail.com>
2025-01-23 05:48:38 +0300

    Update config.py

--- config.py
+++ config.py
@@ -8,6 +8,6 @@ DB_PATH = "repositories.db"
 PER_PAGE = 100  # Number of results per page
 POLLING_INTERVAL = 3600  # 1 hour in seconds
 START_DATE = "2022-01-01T00:00:00Z"  # ISO date format for commits
-#GITHUB_TOKEN = os.getenv("")  #please, uncomment it and complete with your personal GitHub token 
+#GITHUB_TOKEN = os.getenv("")  #please, uncomment this line and complete with your personal GitHub token 
 if not GITHUB_TOKEN:
   raise ValueError("Missing GITHUB_TOKEN in environment variables.")
